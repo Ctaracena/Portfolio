79215d95540f46595d7b98ee51557a3de042c46f
Carlos Taracena <charlietaracenadrums@gmail.com>
2020-09-07 10:25:45 -0500

    mejorada la animación del menú mobile

--- script/index.js
+++ script/index.js
@@ -1,7 +1,9 @@
 const backBtn = document.querySelector('.back')
 const menu = document.querySelector('.burguer-btn')
+const img = document.querySelector('.menu-btn')
 const mobile = document.querySelector('.mobile')
 const navMobile = document.querySelector('.nav-mobile')
+const menuList = document.querySelector('.menu-list')
 
 backBtn.addEventListener('click', () => {
   document.documentElement.scrollTop = 0
@@ -13,9 +15,17 @@ menu.addEventListener('click', () => {
   if (show == false) {
     mobile.style.display = 'flex'
     navMobile.classList.add('slide-in-top')
+    img.src = "images/close.png"
     show = true
   } else {
     mobile.style.display = 'none'
+    img.src = "images/menu.png"
     show = false
   }
+})
+
+menuList.addEventListener('click', () => {
+  mobile.style.display = 'none'
+  img.src = "images/menu.png"
+  show = false
 })
